d7d26e318b90fc11cbfdfa4cc81902de24a5173b
iam-subho <55249273+iam-subho@users.noreply.github.com>
2023-09-21 10:00:41 +0530

    Material Extended Floating button used and some with auto hide during scroll

--- app/src/main/java/kundu/subhojit/moneytracker/module/accounts/FragmentAccountList.java
+++ app/src/main/java/kundu/subhojit/moneytracker/module/accounts/FragmentAccountList.java
@@ -2,23 +2,25 @@ package kundu.subhojit.moneytracker.module.accounts;
 
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 
 import kundu.subhojit.moneytracker.R;
 import kundu.subhojit.moneytracker.database.DatabaseHelper;
 import kundu.subhojit.moneytracker.database.entity.AccountEntity;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 
 public class FragmentAccountList extends Fragment {
@@ -39,6 +41,24 @@ public class FragmentAccountList extends Fragment {
         View accountListView= inflater.inflate(R.layout.fragment_account_list, container, false);
 
         ListView listaccount=accountListView.findViewById(R.id.listacccount);
+        ExtendedFloatingActionButton floatingButton = requireActivity().findViewById(R.id.add_fab);
+
+        listaccount.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem + visibleItemCount >= totalItemCount) {
+                     floatingButton.hide();
+                } else {
+                    floatingButton.show();
+                }
+            }
+        });
+
+
         DatabaseHelper databaseHelper=DatabaseHelper.getInstance(mcontext);
         ArrayList<AccountEntity> list=(ArrayList)databaseHelper.accountDao().getAllAccounts();
         adapter=new AccountsAdapter(requireActivity(),mcontext,list);
--- app/src/main/java/kundu/subhojit/moneytracker/module/home/DashboardActivity.java
+++ app/src/main/java/kundu/subhojit/moneytracker/module/home/DashboardActivity.java
@@ -1,5 +1,19 @@
 package kundu.subhojit.moneytracker.module.home;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -7,25 +21,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.Toast;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationBarView;
 
 import kundu.subhojit.moneytracker.R;
 import kundu.subhojit.moneytracker.database.DatabaseHelper;
-import kundu.subhojit.moneytracker.database.entity.AccountTypeEntity;
-import kundu.subhojit.moneytracker.module.BaseActivity;
+import kundu.subhojit.moneytracker.database.entity.AccountEntity;
 import kundu.subhojit.moneytracker.module.common.DueLoanFragment;
 import kundu.subhojit.moneytracker.module.common.IncomeExpenseFragment;
 import kundu.subhojit.moneytracker.module.common.SettingsFragment;
@@ -37,6 +39,10 @@ public class DashboardActivity extends AppCompatActivity {
     FloatingActionButton fab;
     Button homebtn,incomebtn,expensebtn,settingsbtn;
     private BottomNavigationView bottomNavigationView;
+    ExtendedFloatingActionButton mAddFab;
+    FloatingActionButton mAddIncomeExpense, mAddBank;
+    TextView addaccountTV,addincomeexpenseTV;
+    Boolean isAllFabsVisible;
     Utility utility;
     String[] permissionsRequired = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
@@ -44,16 +50,71 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard); // Set your layout resource here
-        fab=findViewById(R.id.fabtn);
+        setContentView(R.layout.new_dashboard); // Set your layout resource here
+        //fab=findViewById(R.id.fabtn);
         utility=new Utility(DashboardActivity.this);
         setuppermission();
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        /*SpeedDialView speedDialView=findViewById(R.id.floatingActionBtn);
+
+        speedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
+            @Override
+            public boolean onActionSelected(SpeedDialActionItem actionItem) {
+                int actiod=actionItem.getId();
+                switch (actiod){
+                    case R.id.bankadd:
+                        Toast.makeText(DashboardActivity.this, "Bank Button Click", Toast.LENGTH_SHORT).show();
+                    case R.id.transactionadd:
+                        Toast.makeText(DashboardActivity.this, "Transaction Add Click", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });*/
+
+        mAddFab = findViewById(R.id.add_fab);
+        mAddIncomeExpense=findViewById(R.id.add_incomeexpense);
+        mAddBank=findViewById(R.id.add_accounts);
+        addaccountTV=findViewById(R.id.add_accounts_text);
+        addincomeexpenseTV=findViewById(R.id.add_incomeexpense_text);
+
+        mAddIncomeExpense.setVisibility(View.GONE);
+        addincomeexpenseTV.setVisibility(View.GONE);
+        mAddBank.setVisibility(View.GONE);
+        addaccountTV.setVisibility(View.GONE);
+        isAllFabsVisible = false;
+        mAddFab.shrink();
+
+        mAddFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isAllFabsVisible){
+                    showallmenu();
+                }else{
+                    hideallmenu();
+                }
+            }
+        });
+
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FabMenuFragment fabMenuFragment = new FabMenuFragment(DashboardActivity.this);
                 fabMenuFragment.show(getSupportFragmentManager(), fabMenuFragment.getTag());
+            }
+        });*/
+
+        mAddIncomeExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideallmenu();
+                openincomeexpense();
+            }
+        });
+        mAddBank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideallmenu();
+                openaddbankaccount();
             }
         });
 
@@ -90,6 +151,51 @@ public class DashboardActivity extends AppCompatActivity {
         //bottomNavigationView.setSelectedItemId(R.id.home);
     }
 
+
+
+    private void openaddbankaccount() {
+        Dialog dialog=new Dialog(DashboardActivity.this);
+        dialog.setContentView(R.layout.dialog_add_account);
+        // Set the width of the dialog to match the device's width
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.copyFrom(dialog.getWindow().getAttributes());
+        layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        // Apply the width settings to the dialog
+        dialog.getWindow().setAttributes(layoutParams);
+
+        Button addButton = dialog.findViewById(R.id.addAccount);
+        EditText accountNameEditText = dialog.findViewById(R.id.accountNameET);
+        EditText balanceEditText = dialog.findViewById(R.id.balanceET);
+        Spinner accountType =dialog.findViewById(R.id.accountType);
+
+        dialog.show();
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String accountName=accountNameEditText.getText().toString();
+                String balance=balanceEditText.getText().toString();
+                int accountTypeidint=accountType.getSelectedItemPosition();
+                String accountTypeid= String.valueOf(accountTypeidint);
+
+
+                Log.e(TAG,"Data are "+accountName+" "+balance);
+
+                DatabaseHelper databaseHelper=DatabaseHelper.getInstance(DashboardActivity.this);
+
+                databaseHelper.accountDao().insertAccount(new AccountEntity(accountName,balance,accountTypeid));
+
+                Toast.makeText(DashboardActivity.this,"Account Added",Toast.LENGTH_SHORT);
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private void openincomeexpense() {
+    }
+
     public void fragmentopener(Fragment fragment, @NonNull Boolean flag){
 
         FragmentManager fragmentManager=getSupportFragmentManager();
@@ -116,4 +222,24 @@ public class DashboardActivity extends AppCompatActivity {
             utility.setPrefBoolean(Constants.permissionStatus, true);
         }
     }
+
+    public void hideallmenu(){
+        mAddIncomeExpense.setVisibility(View.GONE);
+        addincomeexpenseTV.setVisibility(View.GONE);
+        mAddBank.setVisibility(View.GONE);
+        addaccountTV.setVisibility(View.GONE);
+        isAllFabsVisible = false;
+        mAddFab.shrink();
+    }
+
+    private void showallmenu() {
+        mAddIncomeExpense.setVisibility(View.VISIBLE);
+        addincomeexpenseTV.setVisibility(View.VISIBLE);
+        mAddBank.setVisibility(View.VISIBLE);
+        addaccountTV.setVisibility(View.VISIBLE);
+        mAddFab.extend();
+        isAllFabsVisible=true;
+    }
+
+
 }
